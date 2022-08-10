0ff2676235451a99add7828084d9118f43b053a6
Mirlan Zhalilov <mirlan.zhalilov280289@gmail.com>
2022-08-10 18:01:30 +0300

    fixed

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 let a='';
 let b='';
-let sign=''
+let sign='';
 let finish=false;
 
-const digit=['0','1','2','3','4','5','6','7','8','9','.'];
-const action=['-','+','x','/'];
+const digit= ['0','1','2','3','4','5','6','7','8','9','.'];
+const action= ['-','+','x','/'];
 
 const out=document.querySelector('.calc-screen p');
-function clearAll(){
+ 
+function clearAll() {
     a='';
     b='';
-    sign=''
+    sign='';
     finish=false;
     out.textContent=0;
 }
 document.querySelector('.ac').onclick=clearAll;
 
 document.querySelector('.buttons').onclick=(event)=>{
-    if (!event.target.classList.contains('.btn')) return;
+    if (!event.target.classList.contains('btn')) return;
     if (event.target.classList.contains('ac')) return;
 
 out.textContent='';
